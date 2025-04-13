7b5733929b43895a737ef10ac05ac49609f9d0c9
Marc Jannis Nöthen <marcjannis89@gmail.com>
2025-04-13 09:02:07 +0200

    Add files via upload

---
+++ project_codes/Backend/CoffeeMachine/CoffeeMachine.py
@@ -0,0 +1,37 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# TODO 1. Frage den User was er haben will. Espresso / Latte / Cappuccino
+# TODO 1.1 Überprüfe die eingabe des User und entscheide was als nächstes getan werden soll
+# TODO 1.2 Die Eingabeaufforderung sollte immer anzeigen, wenn eine Aktion beendet wurde.
+# TODO 1.2.1 Sobald das Getränk ausgegeben wurde. Die Eingabeaufforderung sollte dann den nächsten Kunde bedienen
+
