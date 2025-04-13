594f82d1e2ac6d2886f61d2a5200196ff790a41f
Marc Jannis Nöthen <marcjannis89@gmail.com>
2025-04-13 08:59:24 +0200

    Add files via upload

---
+++ project_codes/Backend/coding/Debug_Testing.py
@@ -0,0 +1,25 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+print(display)
+
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Bitte geben Sie ein Buchstaben ein: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win !")
