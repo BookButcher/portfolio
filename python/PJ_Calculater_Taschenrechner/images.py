dc437c6cb7be905ee7f724342cad9ffe111d894c
Marc Jannis Nöthen <marcjannis89@gmail.com>
2025-04-14 10:39:36 +0200

    Add files via upload

---
+++ python/PJ_Calculater_Taschenrechner/images.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+root = Tk()
+root.title('Learn to Code ')
+root.iconbitmap('c:/gui/codemy.ico')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+button_quit = Button(root, text="Exit Programm", command=root.quit)
+button_quit.pack()
+
+
+root.mainloop()
